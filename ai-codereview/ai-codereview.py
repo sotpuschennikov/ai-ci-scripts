fac221f1ee5a9597051c0d0e0956e2fda6f7486a
sotpuschennikov <sotpuschennikov@mirantis.com>
2025-03-12 20:10:35 +0300

    Update ai-codereview.py

--- ai-codereview/ai-codereview.py
+++ ai-codereview/ai-codereview.py
@@ -56,12 +56,8 @@ def main(args):
             temperature=args.temperature,
             max_tokens=args.max_tokens,
         )
-        f = open("output.txt", "a")
-        f.write(re.sub(r'<think>.*?</think>', '', chat_completion.choices[0].message.content, flags=re.DOTALL))
-        print(f.read())
-        f.close()
-        #print("Output: ", re.sub(r'<think>.*?</think>', '', chat_completion.choices[0].message.content, flags=re.DOTALL))
-        #print("Usage: ", chat_completion.usage)
+        print("Output: ", re.sub(r'<think>.*?</think>', '', chat_completion.choices[0].message.content, flags=re.DOTALL))
+        print("Usage: ", chat_completion.usage)
     except Exception as e:
         print(f"Error accessing OpenAI API: {e}")
         sys.exit(1)
