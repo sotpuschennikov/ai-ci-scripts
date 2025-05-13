1d2a6869e382efa4899605e0d3137b68e5468560
sotpuschennikov <perssv83@gmail.com>
2025-05-13 20:34:08 +0400

    Update prompt for summary in  ai-codereview.py

--- ai-codereview/ai-codereview.py
+++ ai-codereview/ai-codereview.py
@@ -61,7 +61,7 @@ def main(args):
         f = open("./output.txt", "a")
         f.write(re.sub(r'<think>.*?</think>', '', chat_completion.choices[0].message.content, flags=re.DOTALL))
         f.close()
-        prompt_summary = 'Using markdown format briefly describe this review: ' + output
+        prompt_summary = 'Using markdown format briefly describe this review in 5 lines: ' + output
         chat_completion_summary = client.chat.completions.create(
             model=MODEL,
             messages=[
